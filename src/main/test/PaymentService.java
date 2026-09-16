74b9e05d3d8ae684e5b05412b5b6c8bdb19314ad
Diego Akira Alvarado Arroyo <dalvaraa@emeal.nttdata.com>
2026-09-16 14:53:07 -0500

    revert(baseline): remove undeployed production logic
    Production-Tag: prod-v1
    Production-Commit: 2bfd819d1217159baf2c35f229edb1651815ca93
    Source-Analyzed-Head: 38afb1a2596ee2e39aabf2e95b8ad593cf8e5a1e
    Reversed-Commits: 38afb1a2596ee2e39aabf2e95b8ad593cf8e5a1e

--- src/main/test/PaymentService.java
+++ src/main/test/PaymentService.java
@@ -3,7 +3,6 @@ package com.test;
 public class PaymentService {
 
     public double calculateTotal(double amount) {
-        double tax = amount * 0.19;
-        return amount + tax;
+        return amount;
     }
 }
